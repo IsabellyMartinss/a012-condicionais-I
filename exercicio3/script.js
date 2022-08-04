023836d9e024744e230908c5ba8641c8e015a8cc
Isabelly Martins <isabellyksml@gmail.com>
2022-08-04 19:22:29 -0300

    Exercício de Fixação COndicionais A012 - Concluídos

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,22 @@
-let nacionalidade //= adicione o prompt aqui
+let nacionalidade = prompt('Qual é a sua nacionalidade?');
+
+let resposta = nacionalidade;
+
+if(resposta == 'Brasileira'){ 
+    console.log('Brasileira')
+}else if(resposta == 'Argentina'){
+	console.log('Argentina')
+}else if(resposta == 'Uruguaia'){ 
+    console.log('Uruguaia')
+}else if(resposta == 'Chilena'){
+    console.log('Chilena')
+}else if(resposta == 'Colombiana'){
+    console.log('Colombiana')
+}else{
+    console.log('Nacionalidade inválida')
+};
+    
+
+
+
+
